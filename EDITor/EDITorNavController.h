edb3ccff6e647efa5be19aae5f39f61bbb91be45
Erwin Mombay <berwin9@yahoo.com>
2012-08-20 00:18:19 -0400

    added custom class for navcontroller

---
+++ EDITor/EDITorNavController.h
@@ -0,0 +1,13 @@
+//
+//  EDITorNavController.h
+//  EDITor
+//
+//  Created by Erwin Mombay on 8/19/12.
+//  Copyright (c) 2012 win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDITorNavController : UINavigationController
+
+@end
---
+++ EDITor/EDITorNavController.m
@@ -0,0 +1,43 @@
+//
+//  EDITorNavController.m
+//  EDITor
+//
+//  Created by Erwin Mombay on 8/19/12.
+//  Copyright (c) 2012 win. All rights reserved.
+//
+
+#import "EDITorNavController.h"
+
+@interface EDITorNavController ()
+
+@end
+
+@implementation EDITorNavController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
